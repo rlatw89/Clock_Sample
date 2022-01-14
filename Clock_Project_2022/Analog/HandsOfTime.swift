f2815887a674cbf931f66386a25fad02428fd4e0
rlatw89 <rlatw89@gmail.com>
2022-01-14 21:26:00 +0900

    first commit

--- Clock_Project_2022/Analog/HandsOfTime.swift
+++ Clock_Project_2022/Analog/HandsOfTime.swift
@@ -5,4 +5,65 @@
 //  Created by Taewan_MacBook on 2022/01/14.
 //
 
-import Foundation
+import UIKit
+
+enum HandsOfTimeType {
+    case hour
+    case minute
+    case second
+    
+    var capacity: Int {
+        switch self {
+        case .hour: return 12
+        case .minute: return 60
+        case .second: return 60
+        }
+    }
+    
+    var width: CGFloat {
+        switch self {
+        case .hour: return 10
+        case .minute: return 5
+        case .second: return 2
+        }
+    }
+    
+    var heightRatio: CGFloat {
+        switch self {
+        case .hour: return 1 / 2
+        case .minute: return 2 / 3
+        case .second: return 5 / 6
+        }
+    }
+}
+
+class HandsOfTime: CAShapeLayer {
+    
+    var type: HandsOfTimeType = .second
+    
+    convenience init(type: HandsOfTimeType) {
+        self.init()
+        self.type = type
+    }
+    
+    func drawPath(value: Int, mid: CGPoint, radius: CGFloat) {
+        let path = UIBezierPath()
+        let value = value % type.capacity
+        let radius = radius * type.heightRatio
+        
+        let angle = 2 * CGFloat.pi * CGFloat(value) / CGFloat(type.capacity)
+        let yPos = mid.y - radius * cos(angle)
+        let xPos = mid.x + radius * sin(angle)
+        
+        path.move(to: mid)
+        path.addLine(to: CGPoint(x: xPos, y: yPos))
+        path.close()
+        
+        path.lineWidth = type.width
+        
+        self.path = path.cgPath
+        self.lineWidth = type.width
+        self.strokeColor = type == .second ? UIColor.red.cgColor : UIColor.black.cgColor
+    }
+    
+}
--- Clock_Project_2022/Digital/DigitalUnitView.swift
+++ Clock_Project_2022/Digital/DigitalUnitView.swift
@@ -5,4 +5,57 @@
 //  Created by Taewan_MacBook on 2022/01/14.
 //
 
-import Foundation
+import UIKit
+
+/// Combined two digital number view
+class DigitalUnit: UIView {
+    private lazy var leftDigit: DigitalComponent = {
+        let digit = DigitalComponent()
+        digit.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(digit)
+        return digit
+    } ()
+    
+    private lazy var rightDigit: DigitalComponent = {
+        let digit = DigitalComponent()
+        digit.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(digit)
+        return digit
+    } ()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    /// draw left digit and right digit by input value
+    /// - Parameter value: time component. hour, minute or second.
+    func setSegmentNumbers(value: Int) {
+        // set left digit and right digit
+        let left = value / 10
+        let right = value - left * 10
+        leftDigit.setSegmentNumber(value: left)
+        rightDigit.setSegmentNumber(value: right)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            leftDigit.leadingAnchor.constraint(equalTo: leadingAnchor),
+            leftDigit.topAnchor.constraint(equalTo: topAnchor),
+            leftDigit.bottomAnchor.constraint(equalTo: bottomAnchor),
+            leftDigit.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5)
+        ])
+        
+        NSLayoutConstraint.activate([
+            rightDigit.leadingAnchor.constraint(equalTo: leftDigit.trailingAnchor),
+            rightDigit.topAnchor.constraint(equalTo: topAnchor),
+            rightDigit.bottomAnchor.constraint(equalTo: leftDigit.bottomAnchor),
+            rightDigit.widthAnchor.constraint(equalTo: leftDigit.widthAnchor)
+        ])
+    }
+}
+
