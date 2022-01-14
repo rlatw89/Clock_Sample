5b43f7728bdfdd768a7a7f6dd80a7eb3f6e4047d
rlatw89 <rlatw89@gmail.com>
2022-01-14 21:02:23 +0900

    Initial Commit

---
+++ Clock_Project_2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clock_Project_2022
+//
+//  Created by Taewan_MacBook on 2022/01/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Clock_Project_2022UITests/Clock_Project_2022UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Clock_Project_2022UITestsLaunchTests.swift
+//  Clock_Project_2022UITests
+//
+//  Created by Taewan_MacBook on 2022/01/14.
+//
+
+import XCTest
+
+class Clock_Project_2022UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
